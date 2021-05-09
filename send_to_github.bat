4be412901e691b929239b4878577c850a7f56136
unknown <mepacheco+github@gmail.com>
2021-05-10 01:16:02 +0200

    10/05/2021_13227

--- send_to_github.bat
+++ send_to_github.bat
@@ -1,7 +1,12 @@
 @echo off
 cd src
 git add --all
+if [%1]==[] goto :blank
 git commit -am %date%_%random%
+goto :continuar
+:blank
+git commit -am %date%_%random%
+:continuar
 git push
 cd ..
 pause
